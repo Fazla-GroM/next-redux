3da68d2afb0fcc685a04a43d5c3502c04fa1aa64
Saša Fazlić <45801671+Fazla-GroM@users.noreply.github.com>
2025-05-06 17:13:06 +0200

    feat(feed): implement infinite scroll (#5)

--- src/features/posts/ui/post-list.tsx
+++ src/features/posts/ui/post-list.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { Stack } from "@chakra-ui/react";
+import { HStack, Show, Spinner, Stack, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
-import { useGetPostListQuery } from "../redux/posts-api-slice";
+import { useGetPostListInfiniteQuery } from "../redux/posts-api-slice";
 import { PostCard } from "./post-card";
 import { PostCardSkeleton } from "./post-card-skeleton";
 
 export function PostList() {
-    const { data, error, isLoading } = useGetPostListQuery({
-        search: {
-            limit: 20,
-        },
-    });
+    const { ref, inView } = useInView();
+    console.log("PostList inView", inView);
+
+    const { data, isLoading, isError, isFetchingNextPage, hasNextPage, fetchNextPage } =
+        useGetPostListInfiniteQuery();
+
+    const flattenedPosts = data?.pages.flatMap((item) => item.posts);
+
+    useEffect(() => {
+        if (inView) {
+            void fetchNextPage();
+        }
+    }, [fetchNextPage, inView]);
 
     if (isLoading) {
         return (
@@ -47,13 +57,13 @@ export function PostList() {
         );
     }
 
-    if (error) {
-        return <div>Error: {JSON.stringify(error)}</div>;
+    if (isError) {
+        return <div>Error</div>;
     }
 
     return (
         <Stack as="ul" gap="4">
-            {data?.posts.map((post) => (
+            {flattenedPosts?.map((post) => (
                 <li key={post.id}>
                     <PostCard
                         title={post.title}
@@ -64,6 +74,20 @@ export function PostList() {
                     />
                 </li>
             ))}
+            <Show
+                when={hasNextPage}
+                fallback={
+                    <HStack justifyContent="center" height="11">
+                        <Text textStyle="sm">No more posts.</Text>
+                    </HStack>
+                }
+            >
+                <HStack ref={ref} justifyContent="center" height="11">
+                    <Show when={isFetchingNextPage}>
+                        <Spinner size="lg" />
+                    </Show>
+                </HStack>
+            </Show>
         </Stack>
     );
 }
